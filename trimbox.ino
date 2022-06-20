714a34139b495ac7ddec17c228987621a708c5c1
Harris Enniss <henniss@google.com>
2022-06-20 16:24:48 -0700

    Add window-smoothing and joystick

--- trimbox.ino
+++ trimbox.ino
@@ -15,24 +15,35 @@
 #define REG_CONV 0b00
 #define REG_CONFIG 0b01
 
+#define WINDOW_SIZE 2
+#define WINDOW_H (WINDOW_SIZE ? (0b1 << (WINDOW_SIZE - 1)) : 0)
+#define WINDOW_L (WINDOW_SIZE ? (-WINDOW_H + 1) : 0)
+
 // Forward Declarations.
 // Wouldn't normally be necessary, but arduino's IDE seems to sometimes insert these in the wrong place.
 typedef uint8_t error_t;
 typedef struct {
   uint8_t addr, mask;
   uint16_t config;
+  // Raw state of analog reads.
   int16_t a[4];
+  // Smoothed state of analog reads.
+  int16_t wa[4];
 } ads1115_state;
 
 void dprintf(const char *format, ...);
 
-error_t read_config(ads1115_state *adc);
+
+error_t prepareAdc(ads1115_state *adc);
+error_t readConfig(ads1115_state *adc);
+error_t writeConfig(ads1115_state *adc, uint16_t config);
 // Split the config word on sections into a null-terminated string.
 const char* bitConfigz(const ads1115_state *adc);
 // Parse the config word into a null-terminated string.
 const char* humanConfigz(const ads1115_state *adc);
 error_t readAdc(ads1115_state *adc, uint8_t idx);
 error_t readAll(ads1115_state *adc);
+error_t updateWindows(const ads1115_state *adc, int16_t *wa);
 
 // Begin Definitions.
 
@@ -52,18 +63,16 @@ void dprintf(const char * format, ...) {
 }
 
 // ADC section.
-// global state.
-ads1115_state adc {.addr = ADDR, .mask = 0b0011};
 
-// nibble -> char
-#define ntoc(n) \
+// half-nibble -> char
+#define hntoc(n) \
   ( n & 0b100 ? (n & 0b010 ? (n & 0b001 ? '7' : '6') : (n & 0b001 ? '5' : '4') ) : (n & 0b010 ? (n & 0b001 ? '3' : '2') : (n & 0b001 ? '1' : '0') ))
 
 // Loads the configuration presently on the device, setting adc->config
-error_t read_config(ads1115_state *adc) {
+error_t readConfig(ads1115_state *adc) {
   error_t err;
   if (adc == NULL) {
-    dprintf("read_config(): NULL pointer");
+    dprintf("readConfig(): NULL pointer");
     return 1;
   }
 
@@ -83,21 +92,56 @@ error_t read_config(ads1115_state *adc) {
     adc->config |= Wire.read();
   }
   else {
-    dprintf("read_config(): wire not available.");
+    dprintf("readConfig(): wire not available.");
     return 2;
   }
   return 0;
 }
 
+error_t writeConfig(ads1115_state *adc, uint16_t config) {
+  error_t err;
+  if (adc == NULL) {
+    dprintf("readConfig(): NULL pointer");
+    return 1;
+  }
+
+  Wire.beginTransmission(adc->addr);
+  Wire.write(REG_CONFIG);
+  Wire.write(0xff & (config >> 8));
+  Wire.write(0xff & (config >> 0));
+  err = Wire.endTransmission(adc->addr);
+  if (err != 0) {
+    dprintf("Wire.endTransmission(): couldn't write config: %d", err);
+    return err;
+  }
+
+#if DEBUG
+  // This check is expensive; only do it in debug mode.
+  err = readConfig(adc);
+  if (err != 0) {
+    dprintf("writeConfig(): Unexpected failure checking result.");
+    return err;
+  }
+  // skip bit 15; it has unique R/W semantics.
+  if ((config & 0x7fff) != (adc->config & 0x7fff)) {
+    dprintf("writeConfig(): Mismatched result.");
+    dprintf("expected: %s", bitConfigz(config));
+    dprintf("got:      %s", bitConfigz(adc->config));
+  }
+#endif
+  adc->config = (config & 0x7fff);
+}
+
+
 // DEBUG only.
 // Split the config word on sections into a null-terminated string.
-const char* bitConfigz(const ads1115_state *adc) {
+const char* bitConfigz(uint16_t config) {
 #if DEBUG
   static char buf[26];
   uint8_t wh = 0;
   for (int8_t i = 15; i >= 0; --i) {
     // post-increment, i.e. write to the vacated index value.
-    buf[wh++] = ((adc->config >> i) & 0b1) ? '1' : '0';
+    buf[wh++] = ((config >> i) & 0b1) ? '1' : '0';
     // Don't increment.
     buf[wh] = '\0';
     switch (i) {
@@ -129,14 +173,14 @@ ret:
 
 // DEBUG only.
 // Parse the config word into a null-terminated string.
-const char* humanConfigz(const ads1115_state *adc) {
+const char* humanConfigz(uint16_t config) {
 #if DEBUG
   static char buf[26];
   char scratch[16];
   uint8_t wh = 0, val = 0, b = 0;
   for (int8_t i = 15; i >= 0; --i) {
     val <<= 1;
-    val |= ((adc->config >> i) & 0b1);
+    val |= ((config >> i) & 0b1);
     switch (i) {
       // Terminal bits of each section, from p18 of the ADS1115 datasheet.
       case 15:
@@ -146,12 +190,12 @@ const char* humanConfigz(const ads1115_state *adc) {
         if (val & 0b100) {
           // Absolute
           buf[wh++] = 'A';
-          buf[wh++] = ntoc(val & 0b011);
+          buf[wh++] = hntoc(val & 0b011);
           buf[wh++] = 'G';
         }
         else if (val) {
           buf[wh++] = 'D';
-          buf[wh++] = ntoc((val & 0b011) - 1);
+          buf[wh++] = hntoc((val & 0b011) - 1);
           buf[wh++] = '3';
         }
         else {
@@ -246,12 +290,25 @@ ret:
 #endif
 }
 
+error_t prepareAdc(ads1115_state *adc) {
+  uint16_t config;
+  error_t err;
+  if (adc == NULL) {
+    dprintf("prepareAdc(): NULL pointer");
+    return 1;
+  }
+  // Don't bother configuring input mutex: that's set per-read.
+  //         4V gain      single-shot      128SPS     disable comparator
+  config = ((0b001 << 9) | (0b1 << 8) | (0b100 << 5) |  (0b11 << 0));
+  writeConfig(adc, config);
+}
+
 
 error_t readAdc(ads1115_state *adc, uint8_t idx) {
   uint16_t config;
   error_t err;
   if (adc == NULL) {
-    dprintf("read_adc(): NULL pointer");
+    dprintf("readAdc(): NULL pointer");
     return 1;
   }
   // Clamp to 0-3
@@ -262,32 +319,18 @@ error_t readAdc(ads1115_state *adc, uint8_t idx) {
     return 0;
   }
 
-  // Don't want to commit this until write succeedes.
   config = adc->config;
-
   // Zero out mux config.
   config &= ~(0b111 << 12);
   // Absolute mode only.
   config |= (0b1 << 14);
   config |= (idx << 12);
-
-
-  // Zero out PGA config.
-  config &= ~(0b111 << 9);
-  // Drop gain.
-  config |= (0b001 << 9);
-
-  Wire.beginTransmission(adc->addr);
-  Wire.write(byte(REG_CONFIG));
-  // Write with conversion bit set.
-  Wire.write(byte(0x80 | (config >> 8)));
-  Wire.write(byte(config & 0xff));
-  err = Wire.endTransmission(adc->addr);
+  // Request conversion
+  config |= (0b1 << 15)
+            err = writeConfig(adc, config);
   if (err != 0) {
-    dprintf("Wire.endTransmission(): couldn't write config: %d", err);
     return err;
   }
-  adc->config = config;
 
   // Todo(tune this)
   delay(70);
@@ -326,25 +369,62 @@ error_t readAll(ads1115_state *adc) {
       return err;
     }
   }
+  updateWindows(adc);
   return 0;
 }
 
-void setup() {
-  delay(4000);
-  uint8_t err;
-  uint16_t config, dat;
+void updateWindows(ads1115_state * adc) {
+  uint16_t r, o;
+  for (uint8_t i = 0; i < 4; ++i) {
+    r = adc->a[i];
+    o = adc->wa[i];
 
-  dprintf("Reading config...");
-  err = read_config(&adc);
+    // if this would overflow/underflow, then no need to move window in respective direction. 
+    if ((INT16_MIN - WINDOW_L > o) && (r < (o + WINDOW_L))) {
+      wa[i] = r - WINDOW_L;
+    }
+    else if ((INT16_MAX - WINDOW_H < o) && (r > (o + WINDOW_H))) {
+      wa[i] = r - WINDOW_H;
+    }
+  }
+}
+
+// Axes
+
+void joystickInit() {
+#if DEBUG
+#else
+  Joystick.begin();
+}
+
+// Encoders
+// Button Matrix
+
+
+// Global state.
+ads1115_state adc {.addr = ADDR, .mask = 0b0011};
+Joystick_ joystick;
+
+// Entrypoints.
+
+void setup() {
+#if DEBUG
+  // Wait a little for the serial console to become available.
+  delay(4000);
+#endif
+  uint8_t err;
+
+  dprintf("Preparing config...");
+  err = prepareAdc(&adc);
   if (err != 0) {
     return;
   }
-  dprintf(bitConfigz(&adc));
-  dprintf(humanConfigz(&adc));
+  dprintf(bitConfigz(adc.config));
+  dprintf(humanConfigz(adc.config));
 
   err = readAll(&adc);
   if (err != 0) {
-    err = read_config(&adc);
+    err = readConfig(&adc);
     if (err != 0) {
       return;
     }
@@ -355,8 +435,10 @@ void setup() {
     dprintf("A%d:%d", i, adc.a[i]);
   }
 
-  dprintf(bitConfigz(&adc));
-  dprintf(humanConfigz(&adc));
+  dprintf(bitConfigz(adc.config));
+  dprintf(humanConfigz(adc.config));
+
+  joystickInit();
 
   dprintf("done");
 }
